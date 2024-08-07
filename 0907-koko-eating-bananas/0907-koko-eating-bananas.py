d4e8c9e5c95bda824609c9ba019221eabe0499b6
insaneDX <50282409+insaneDX@users.noreply.github.com>
2024-08-07 16:55:49 +0530

    Time: 271 ms (47%), Space: 18.1 MB (44.07%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -3,7 +3,7 @@ class Solution:
     def calculateHours(self, piles: List[int], speed: int) -> int:
       hours = 0
       for pile in piles:
-          hours += (pile + speed - 1) // speed 
+          hours += math.ceil(pile/speed) 
       return hours
 
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
