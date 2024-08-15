f86ac972019b432c3d8787e3e65b082af6c3c6c7
insaneDX <50282409+insaneDX@users.noreply.github.com>
2024-08-15 17:18:02 +0530

    Time: 320 ms (9.9%), Space: 24.3 MB (30.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,30 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid: List[List[str]]) -> int:    
         rows = len(grid)
         cols = len(grid[0])
         visited = set()
-        island = 0
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]  # down, right, up, left
+        island = 0
 
-
-        def dfs(row, col):
+        def bfs(row, col):
+            queue = collections.deque()
             visited.add((row, col))
-            for x, y in directions:
-                new_row = row + x
-                new_col = col + y
-                if (new_row in range(rows) and new_col in range(cols) and (new_row, new_col) not in visited and grid[new_row][new_col] == "1"):
-                    dfs(new_row, new_col)
+            queue.append((row, col))
 
+            while queue:
+                current_row, current_col = queue.popleft()
+                for x, y in directions:
+                    new_row = current_row + x
+                    new_col = current_col + y
+
+                    if (new_row in range(rows) and new_col in range(cols) and (new_row, new_col) not in visited) and grid[new_row][new_col] == "1":
+                        queue.append((new_row, new_col))
+                        visited.add((new_row, new_col))
 
         for row in range(rows):
             for col in range(cols):
                 if (row, col) not in visited and grid[row][col] == "1":
+                    bfs(row, col)
                     island += 1
-                    dfs(row, col)
 
         return island
