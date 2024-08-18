c82288ee0a99915ef726c8f489808fc13c8700a5
insaneDX <50282409+insaneDX@users.noreply.github.com>
2024-08-18 19:35:55 +0530

    Time: 562 ms (32.14%), Space: 12 MB (98.21%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -1,57 +1,60 @@
 class UnionFind:
     def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [1] * n
-        self.size = [1] * n
-        self.edge_count = [0] * n
+        self.parent = list(range(n))  # Initialize parent pointers for each node
+        self.rank = [1] * n           # Rank to track depth/max_height of a node
+        self.size = [1] * n           # To track size of a node
+        self.edge_count = [0] * n     # To track edges from a node 
 
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+    def find_parent(self, node):      # Function to find root ancestor
+        curr = node
+        while curr != self.parent[curr]:  # Path compression
+            self.parent[curr] = self.parent[self.parent[curr]] 
+            curr = self.parent[curr]
+        return curr
 
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.parent[rootY] = rootX
-                self.size[rootX] += self.size[rootY]
-                self.edge_count[rootX] += self.edge_count[rootY] + 1
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.parent[rootX] = rootY
-                self.size[rootY] += self.size[rootX]
-                self.edge_count[rootY] += self.edge_count[rootX] + 1
-            else:
-                self.parent[rootY] = rootX
-                self.rank[rootX] += 1
-                self.size[rootX] += self.size[rootY]
-                self.edge_count[rootX] += self.edge_count[rootY] + 1
+    def union(self, u, v):            # Union operation
+        p1, p2 = self.find_parent(u), self.find_parent(v)
+
+        # Merge small component into larger component, increment size and edge count + 1 (extra edge while merging)
+        if p1 != p2:                  # If ancestors are not the same, merge else add 1 edge
+            if self.rank[p1] > self.rank[p2]:
+                self.parent[p2] = p1
+                self.size[p1] += self.size[p2]
+                self.edge_count[p1] += self.edge_count[p2] + 1
+
+            elif self.rank[p1] < self.rank[p2]:
+                self.parent[p1] = p2
+                self.size[p2] += self.size[p1]
+                self.edge_count[p2] += self.edge_count[p1] + 1
+
+            else:                     # If both ranks are the same, merge and increment rank as rank is max_height of tree
+                self.rank[p1] += 1
+                self.parent[p2] = p1
+                self.size[p1] += self.size[p2]
+                self.edge_count[p1] += self.edge_count[p2] + 1
         else:
-            self.edge_count[rootX] += 1
+            self.edge_count[p1] += 1  # If already in the same component, just add an edge
+
 
 class Solution:
     def countCompleteComponents(self, n, edges):
         uf = UnionFind(n)
-        
-        # Perform union operations for all edges
         for u, v in edges:
+            # Perform union find operations
             uf.union(u, v)
-
-        # Count the complete components
-        component_edges = {}
-        for i in range(n):
-            root = uf.find(i)
-            if root not in component_edges:
-                component_edges[root] = 0
-            component_edges[root] = uf.edge_count[root]
         
+        components_edges = {}  # To track parent edges
+        for node in range(n):
+            curr = uf.find_parent(node)
+            if curr not in components_edges:
+                components_edges[curr] = 0
+            components_edges[curr] = uf.edge_count[curr]
+
         complete_components = 0
-        for root in component_edges:
-            size = uf.size[root]
-            edges = uf.edge_count[root]
-            if edges == size * (size - 1) // 2:
+        for node in components_edges:
+            edges = uf.edge_count[node]
+            size = uf.size[node]
+            if edges == size * (size - 1) // 2:  # Formula --> edge = size * (size-1)//2
                 complete_components += 1
         
         return complete_components
