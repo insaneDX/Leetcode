4986342f4983da6b969493d44d1df1273cb9cec7
insaneDX <50282409+insaneDX@users.noreply.github.com>
2025-02-07 17:56:19 +0530

    Time: 371 ms (75.61%), Space: 66.5 MB (75.43%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -1,18 +1,21 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    # Get the unique salaries in descending order
-    unique_salaries = employee['salary'].unique()
-    unique_salaries.sort()
-    unique_salaries = unique_salaries[::-1]
-
-    if len(unique_salaries) < 2:
-        return pd.DataFrame({
-            'SecondHighestSalary':[None]
-        })
-
-    second_highest = unique_salaries[1] # 0th index will be max
-
+    # Sort the salaries in descending order and remove duplicates
+    sorted_unique_salaries = employee['salary'].sort_values(
+        ascending=False
+    ).drop_duplicates()
+    
+    # Return the second highest salary or None if there isn't one
+    second_highest_salary = None if sorted_unique_salaries.size < 2 else sorted_unique_salaries.iloc[1]
+    
     return pd.DataFrame({
-            'SecondHighestSalary':[second_highest]
-        })
+        'SecondHighestSalary': [second_highest_salary]
+    })
+
+# # Write your MySQL query statement below
+# SELECT max(salary) AS SecondHighestSalary 
+# FROM employee 
+# WHERE salary < (
+#     SELECT max(salary) FROM employee
+#     )
