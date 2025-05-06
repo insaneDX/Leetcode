3ef452f39fbfd649dff1298f18edb19525b424f6
insaneDX <50282409+insaneDX@users.noreply.github.com>
2025-05-06 08:32:57 +0530

    Time: 21 ms (23.26%), Space: 22.7 MB (25.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,33 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {}
+        result = defaultdict(list)
 
         for word in strs:
-            key = tuple(sorted(word))  # Convert sorted list to a tuple for using as key
-            if key in hashmap:
-                hashmap[key].append(word)
-            else:
-                hashmap[key] = [word] 
+            count = [0] * 26 # for a to z words
 
-        return list(hashmap.values()) 
+            for char in word:
+                count[ord(char) - ord("a")] += 1 # to make in range(1 to 26)
+            
+            result[tuple(count)].append(word)
+        
+        return list(result.values())
+
+# Total time complexity: O(n × k) where n is the no of string and k is the time of count operation performed on k characters of string
+# Space Complexity: O(n × k)
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         hashmap = {}
+
+#         for word in strs:
+#             key = tuple(sorted(word))  # Convert sorted list to a tuple for using as key
+#             if key in hashmap:
+#                 hashmap[key].append(word)
+#             else:
+#                 hashmap[key] = [word] 
+
+#         return list(hashmap.values()) 
+
+# Time Complexity:O( m.nlogn ), where nlogn for sorting for m words.
+# Space Compleity: O(m * n) where m = number of strings in the list, n = average length of each string
