55176a3c23cedbb180e0cd3cf3b83f21ce83c4a2
insaneDX <50282409+insaneDX@users.noreply.github.com>
2025-05-26 10:07:03 +0530

    Time: 0 ms (100%), Space: 19.1 MB (42.09%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(city):
+            visited[city] = True
+            for neighbor in range(len(isConnected)):
+                if isConnected[city][neighbor] == 1 and not visited[neighbor]:
+                    dfs(neighbor)
+
+        
+        n = len(isConnected)
+        visited = [False] * n
+        provinces = 0
+        for city in range(n):
+            if not visited[city]:
+                dfs(city)
+                provinces += 1
+        
+        return provinces
+
+# class Solution:
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+#         n = len(isConnected)
+#         parent = [i for i in range(n)]
+
+#         def find(x):
+#             if parent[x] != x:
+#                 parent[x] = find(parent[x])
+#             return parent[x]
+        
+#         def union(x, y):
+#             rootx = find(x)
+#             rooty = find(y)
+#             if rootx != rooty:
+#                 parent[rooty] = rootx
+
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 if isConnected[i][j] == 1:
+#                     union(i, j)
+        
+#         # Count number of distinct roots
+#         provinces = len(set(find(i) for i in range(n)))
+#         return provinces
