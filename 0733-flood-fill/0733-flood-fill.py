e7bd97d4c548c9437e867840572fe376750c0991
insaneDX <50282409+insaneDX@users.noreply.github.com>
2025-06-01 13:02:55 +0530

    Time: 0 ms (100%), Space: 18 MB (50.66%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,24 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        pixel_color = image[sr][sc]
-        
+        original_color = image[sr][sc]
+        rows, cols = len(image), len(image[0])
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-        def isValid(row, col):
-            return 0<=row<len(image) and 0<=col<len(image[0])
-        
         def dfs(row, col):
-            if not isValid(row, col) or image[row][col] != pixel_color or image[row][col] == color:
-                print(row, col)
+            if not (0 <= row < rows and 0 <= col < cols) or image[row][col] != original_color:
                 return
             image[row][col] = color
-
             for x, y in directions:
                 dfs(row + x, col + y)
-            
 
+        if original_color != color:
+            dfs(sr, sc)
 
-        dfs(sr, sc)
         return image
-        
