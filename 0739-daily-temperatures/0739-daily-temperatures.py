2807231e4d895760f4477055c3a9d1d3bd6a8587
insaneDX <50282409+insaneDX@users.noreply.github.com>
2026-02-15 17:50:19 +0530

    Time: 89 ms (74.51%), Space: 28.3 MB (65.21%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        result = [0] * n
+        stack = [] # store indices
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            
+            # store unresolved day
+            stack.append(i)
+        
+        return result
