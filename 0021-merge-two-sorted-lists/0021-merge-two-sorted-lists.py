77ea380a4e9f17730b48fa8fca6003604091c400
insaneDX <50282409+insaneDX@users.noreply.github.com>
2026-02-20 15:23:18 +0530

    Time: 3 ms (13.83%), Space: 19.5 MB (27.62%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,28 +5,22 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Create a dummy node to serve as the start of the result list
-        dummy = ListNode(0)
-        curr = dummy
+        curr = ListNode()
+        head = curr
 
-        # Initialize pointers for list1 and list2
-        curr_i = list1
-        curr_j = list2
-
-        # Merge smaller elements first
-        while curr_i and curr_j:
-            if curr_i.val < curr_j.val:
-                curr.next = curr_i
-                curr_i = curr_i.next
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
             else:
-                curr.next = curr_j
-                curr_j = curr_j.next
+                curr.next = list2
+                list2 = list2.next
             curr = curr.next
-
-        #If there are remaining nodes in either list, append them to the result
-        if curr_i:
-            curr.next = curr_i
-        elif curr_j:
-            curr.next = curr_j
-
-        return dummy.next
+        
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        
+        return head.next
+                
