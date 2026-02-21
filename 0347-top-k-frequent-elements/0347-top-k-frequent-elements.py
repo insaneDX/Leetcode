d5a1bb4fe1a332b80ed066d2f344a14e832c42e8
insaneDX <50282409+insaneDX@users.noreply.github.com>
2026-02-21 14:36:03 +0530

    Time: 3 ms (89.96%), Space: 22.9 MB (63.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,31 +4,53 @@ class Solution:
         freqMap = defaultdict(int)
         for num in nums:
             freqMap[num] += 1
-        # Time: O(n)
-        # Space: O(m)  (m = number of unique elements)
-
-        # build max heap
+        
+        # Maintain min heap of size k
         heap = []
         for num, freq in freqMap.items():
-            heapq.heappush(heap, (-freq, num))
-        # Time: O(m log m)
-        # Space: O(m)
+            heapq.heappush(heap, (freq, num))
+            
+            # If heap grows beyond k, remove smallest frequency
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return [num for freq, num in heap]
 
-        result = []
-        for _ in range(k):
-            result.append(heapq.heappop(heap)[1])
-        # Time: O(k log m)
 
-        return result
 
-""" Total time complexity
-O(n)          → build frequency map
-+ O(m log m)  → build heap
-+ O(k log m)  → pop k elements
-"""
-""" Space Complexity
-O(m) → frequency map
-O(m) → heap
-O(k) → result
-So total space O(m)
-"""
+#class Solution:
+#    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#        
+#         freqMap = defaultdict(int)
+#         for num in nums:
+#             freqMap[num] += 1
+#         # Time: O(n)
+#         # Space: O(m)  (m = number of unique elements)
+
+#         # build max heap
+#         heap = []
+#         for num, freq in freqMap.items():
+#             heapq.heappush(heap, (-freq, num))
+#         # Time: O(m log m)
+#         # Space: O(m)
+
+#         result = []
+#         for _ in range(k):
+#             result.append(heapq.heappop(heap)[1])
+#         # Time: O(k log m)
+
+#         return result
+
+# """ Total time complexity
+# O(n)          → build frequency map
+# + O(m log m)  → build heap
+# + O(k log m)  → pop k elements
+# """
+# """ Space Complexity
+# O(m) → frequency map
+# O(m) → heap
+# O(k) → result
+# So total space O(m)
+# """
+
+# What if we used min heap and after k elements we remove the smallest element first which will keep the heap size k. 
