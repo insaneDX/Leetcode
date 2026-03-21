3ae2d6fe0a58a05ff71a9c58eafe09fec84a7f26
Sachin Chaudhary <50282409+insaneDX@users.noreply.github.com>
2026-03-21 11:44:27 +0530

    Time: 27 ms (84.02%), Space: 31.5 MB (62.24%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        curr_sum = nums[0]
+        max_sum = curr_sum
+
+        for right in range(1, len(nums)):
+
+            if curr_sum < 0:
+                curr_sum = nums[right] # resetting the window
+            else:
+                curr_sum += nums[right] # expanding the window
+
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
+
+# At each index:
+
+# Should I:
+# 1. Extend current subarray?
+# 2. Start a new one?
